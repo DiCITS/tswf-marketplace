eae75a0552970b00aed2ee7027569a3ccb9eb6d5
ManuParra <manuparra@iMac-de-iMAC.local>
2020-09-10 18:18:22 +0200

    Minimal About page

--- src/components/features/About/index.js
+++ src/components/features/About/index.js
@@ -1,36 +1,24 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import { BrandButtonDefault } from "../../library/Button"
 
-const UniqueValueStatement = () => (
-    <p className="xs:text-2xl 
-                  sm:text-4xl
-                  md:text-5xl
-                  font-helvetica font-bold text-4xl text-center text-brand-dark leading-snug">
-        Time Series Workflows as a
-        <span className="text-brand"> Cloud Computing </span> 
-        service
-    </p>
+const AuthorContact = ({email}) => (
+   <p>{email}</p>
 )
-const GetStartedButton = () => (
-    <Link to="/signup" tabIndex="-1">
-        <BrandButtonDefault
-            type="button"
-            value="Upload / Compose a Time Series WF"
-            classes="xs:py-3 xs:px-4 xs:text-sm
-                     sm:py-3 sm:px-4 sm:text-sm
-                     w-auto px-8 py-4 mt-8"
-        />
-    </Link>)
 
 const AboutAuthor = () => (
-    <p className="xs:text-2xl 
-                  sm:text-4xl
-                  md:text-5xl
-                  font-helvetica font-bold text-center text-brand-dark leading-snug">
-        Manuel Parra
+    <p className="mx-auto w-1/2 pb-5 px-3 pt-2 mt-2">
+         <span className="font-bold text-brand">Manuel Parra-Royón</span> hold a Ph.D. in Computer Sciences focused in Cloud Computing services and Data Mining.
+        <AuthorContact email="manuel.parra@cern.ch"></AuthorContact>
     </p>
 )    
 
-export default UniqueValueStatement
-export {AboutAuthor}
+const AboutSite= () => (
+    <p className="xs:w-11/12 xs:mt-2 xs:pb-4 
+    sm:w-1/2 sm:mt-4 sm:pb-6
+    md:w-2/5 md:mt-4 md:pb-4
+    lg:w-1/3 lg:mt-4 lg:pb-4
+      mx-auto w-1/2 pb-5 px-3 pt-2 mt-2">                
+        TSWF is an initiative to demonstrate the capability of semantic technologies for the definition and description of services based on Cloud Computing environments for the deployment of work flows with Time Series ready to consume, migrate or deploy independently of the underlying architecture or platform.
+    </p>
+)    
+
+export {AboutAuthor,AboutSite}
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import Statement, { GetStartedButton } from "../../components/features/Statement"
-import {AboutAuthor} from "../../components/features/About"
+import {AboutAuthor,AboutSite} from "../../components/features/About"
 const About = () => (
     <div className="mt-10 flex flex-col items-center pb-24">
+        <AboutSite/>
         <AboutAuthor />
     </div>
 )
