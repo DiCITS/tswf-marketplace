09c0de009636efac6eeeda1649d84d98801a5ab0
ManuParra <manuparra@iMac-de-iMAC.local>
2020-09-10 18:52:50 +0200

    Boom box to include properties as a summary of TSWF

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,11 @@ const Home = () => (
     <div className="mt-10 flex flex-col items-center pb-24">
         <Statement />        
         <GetStartedButton />
+        <div className="h-64 grid grid-rows-1 grid-flow-col gap-4">
+        <div className="bg-gray-200 rounded-lg">Total TSWF</div>
+        <div >Methods included</div>
+        <div >Full service composed</div>
+        </div>
     </div>
 )
 export default Home
